6a353ae350603921cc9fba1b917e19cd68f742e7
Camila <96879526+camilajunges@users.noreply.github.com>
2023-06-25 11:58:53 -0300

    Update MagicEightBall.js

--- MagicEightBall.js
+++ MagicEightBall.js
@@ -1,3 +1,7 @@
 /* In this project we will build a Magic Eight Ball using control flow in JavaScript.
 The user will be able to input a question, then our program will output a random fortune.
 If you get stuck during this project or would like to see an experienced developer work through it, click “Get Unstuck“ to see a project walkthrough video.*/
+
+let userName = '';
+
+userName ? console.log(`Hey! ${userName}`) : console.log('Hello!');
