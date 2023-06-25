6e1758a3bf18a9437123856c7d2e16ee9ffebc54
Camila <96879526+camilajunges@users.noreply.github.com>
2023-06-24 22:04:08 -0300

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,19 @@
+//Minha idade a ser cmparada a idade do cachorro
+const myAge = 18;
+let myName = 'Camila';
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+//console.log(earlyYears);
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+//console.log(laterYears);
+
+let myAgeInDogs = earlyYears + laterYears;
+//console.log(myAgeInDogs);
+
+'Camila'.toLowerCase();
+
+console.log(' ');
+console.log(`Meu nome é ${myName}, eu tenho ${myAge} de idade, o que equivale a ${myAgeInDogs} anos para os cachorros!`);
