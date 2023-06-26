c87c135c6614d844fa183ac84a195b382850e3d0
Camila <96879526+camilajunges@users.noreply.github.com>
2023-06-26 10:52:33 -0300

    Update RaceDay.js

--- RaceDay.js
+++ RaceDay.js
@@ -1,35 +1,26 @@
-/*Aqui está como funciona nosso processo de inscrição. Existem corredores adultos (com mais de 18 anos) e corredores jovens (com menos de 18 anos). Eles podem se inscrever antecipadamente ou tarde. Os corredores recebem um número de corrida e horário de largada com base em sua idade e inscrição.
-
-Número de corrida:
-
-Adultos que se inscrevem antecipadamente recebem um número de corrida igual ou acima de 1000.
-Todos os outros recebem um número abaixo de 1000.
-Horário de largada:
-
-Participantes adultos correm às 9h30 ou 11h00.
-Adultos que se inscrevem antecipadamente correm às 9h30.
-Adultos que se inscrevem tarde correm às 11h00.
-Participantes jovens correm às 12h30 (independentemente da inscrição).
-Mas não planejamos para corredores que têm exatamente 18 anos! Vamos lidar com isso até o final do projeto.
-
-Se você ficar preso durante este projeto ou quiser ver um desenvolvedor experiente trabalhar nele, clique em "Get Unstuck" para assistir a um vídeo com o passo a passo do projeto*/
-
 // Control flows (conditionals) and if/else statements
 
-let raceNumber = Math.floor(Math.random() * 1000);
-let early = true;
-let age = 19;
+let numCorredor = Math.floor(Math.random() * 1000);
+let antecipado = true;
+let idade = 18;
 
 
 // criar um controle de flow que checa se o corredor é adulto e registrado anteriorimente.
 
-if(early && age > 18){ // não precisa especificar se early é true ou false porque a variável já é do tipo boolean
-    raceNumber += 1000;
+if(antecipado && idade > 18){ // não precisa especificar se antecipado é true ou false porque a variável já é do tipo boolean
+    numCorredor += 1000;
 }
 
-// criar um controle de flow separado que vai checar a idade e o registro para determinar a corrida, para corredores abaixo de 18 anos a corrida será às 9:30am, incluindo o raceNumber deles
-if(early && age > 18){
-    console.log(`Race will begin at 9:30am, your race number is: ${raceNumber}.`);
+// criar um controle de flow separado que vai checar a idade e o registro para determinar a corrida, para corredores abaixo de 18 anos a corrida será às 9:30am, incluindo o numCorredor deles
+if(antecipado && idade > 18){
+    console.log(`Sua corrida começará às 9:30am, seu número de corredor(a) é: ${numCorredor}.`);
+} 
+else if(!antecipado && idade > 18){
+    console.log(`Sua corrida começará às 11:00am, seu número de corredor(a) é: ${numCorredor}.`)
+} 
+else if(idade < 18){
+    console.log(`Sua corrida começará às 12:30am, seu número de corredor(a) é: ${numCorredor}.`)
 }
-
-
+else {
+    console.log('Por favor, aproxime-se a mesa de registro.')
+}
