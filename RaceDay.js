39868f6c55d2042f83dd8ebf75bc215ee90e2de6
Camila <96879526+camilajunges@users.noreply.github.com>
2023-06-26 09:38:58 -0300

    Update RaceDay.js

--- RaceDay.js
+++ RaceDay.js
@@ -1,1 +1,17 @@
+/*Aqui está como funciona nosso processo de inscrição. Existem corredores adultos (com mais de 18 anos) e corredores jovens (com menos de 18 anos). Eles podem se inscrever antecipadamente ou tarde. Os corredores recebem um número de corrida e horário de largada com base em sua idade e inscrição.
+
+Número de corrida:
+
+Adultos que se inscrevem antecipadamente recebem um número de corrida igual ou acima de 1000.
+Todos os outros recebem um número abaixo de 1000.
+Horário de largada:
+
+Participantes adultos correm às 9h30 ou 11h00.
+Adultos que se inscrevem antecipadamente correm às 9h30.
+Adultos que se inscrevem tarde correm às 11h00.
+Participantes jovens correm às 12h30 (independentemente da inscrição).
+Mas não planejamos para corredores que têm exatamente 18 anos! Vamos lidar com isso até o final do projeto.
+
+Se você ficar preso durante este projeto ou quiser ver um desenvolvedor experiente trabalhar nele, clique em "Get Unstuck" para assistir a um vídeo com o passo a passo do projeto*/
+
 let raceNumber = Math.floor(Math.random() * 1000);
