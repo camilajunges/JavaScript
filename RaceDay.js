f2c222df0e80c6e103b0cd5f4076edec0b9828ad
Camila <96879526+camilajunges@users.noreply.github.com>
2023-06-26 10:32:33 -0300

    Update RaceDay.js

--- RaceDay.js
+++ RaceDay.js
@@ -14,4 +14,22 @@ Mas não planejamos para corredores que têm exatamente 18 anos! Vamos lidar com
 
 Se você ficar preso durante este projeto ou quiser ver um desenvolvedor experiente trabalhar nele, clique em "Get Unstuck" para assistir a um vídeo com o passo a passo do projeto*/
 
+// Control flows (conditionals) and if/else statements
+
 let raceNumber = Math.floor(Math.random() * 1000);
+let early = true;
+let age = 19;
+
+
+// criar um controle de flow que checa se o corredor é adulto e registrado anteriorimente.
+
+if(early && age > 18){ // não precisa especificar se early é true ou false porque a variável já é do tipo boolean
+    raceNumber += 1000;
+}
+
+// criar um controle de flow separado que vai checar a idade e o registro para determinar a corrida, para corredores abaixo de 18 anos a corrida será às 9:30am, incluindo o raceNumber deles
+if(early && age > 18){
+    console.log(`Race will begin at 9:30am, your race number is: ${raceNumber}.`);
+}
+
+
